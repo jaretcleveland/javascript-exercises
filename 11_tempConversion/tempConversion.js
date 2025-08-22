327ab04e6011918824c2f88bd96a3afc07649175
Jaret Cleveland <81397585+jaretcleveland@users.noreply.github.com>
2025-08-22 18:05:15 -0500

    Complete exercises 06-11
    These exercises were part of the assignment in the 'Loops and
    Arrays' section of the Foundations course.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // Temperature calculation is multiplied by 10 prior to rouding so that it can
+  // later be divided by 10 leaving just one decimal point.
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
