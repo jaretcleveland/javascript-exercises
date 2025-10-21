0aef5f2b84939b3763cc5e670571e7dd76cc6955
Jaret Cleveland <81397585+jaretcleveland@users.noreply.github.com>
2025-10-20 20:03:52 -0500

    Complete exercies within the Object Basics module

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    // Check the argument's type and makes sure it is a number throughout rest of function.
+    let count
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = countArg
+    }
 
+    // Handle edge cases
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    // Initialize the first two Fibonacci numbers
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    // Calculate Fibonacci number iteratively
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+// Returns the age of a person
+const getAge = function(people) {
+    const deathYear = people.yearOfDeath || new Date().getFullYear();
+    return deathYear - people.yearOfBirth;
+};
 
+// Returns the person with the greatest age
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest);
+        const currentAge = getAge(currentPerson);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
 };
 
 // Do not edit below this line
